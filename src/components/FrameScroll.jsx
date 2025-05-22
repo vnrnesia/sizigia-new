7d4fbfd6c959e7fcae44f9ca63e96b514293af6f
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-05-22 17:11:05 +0300

    Optimization test

--- src/components/FrameScroll.jsx
+++ src/components/FrameScroll.jsx
@@ -259,11 +259,7 @@ const FrameScroll = () => {
         >
           <AnimatePresence mode="wait">
             <motion.div
-              key={currentText}
-              initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.1, ease: "easeOut" }}
+            
               className={styles.text}
             >
               {currentText}
